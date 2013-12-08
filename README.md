a07f97ec90d7ab75125b773b76cee50e9cc54982
rucker <rick.ucker@gmail.com>
2013-12-08 17:28:37 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+Chrono: a simple Pebble app I wrote for a marriage proposal.
+Written by Rick Ucker (rucker@github)
+=================
+PURPOSE
+-----------------
+The purpose was simple: I wanted a way to propose to my girlfriend that was 1) romantic 2) not TOO romantic 3) played on common interests of ours and 4) was a little geeky. 
+
+To meet these goals, I designed Chrono. I chose to do this for the reasons above, as well as the fact that the Pebble was a birthday gift from my fiancée. Finally, pulling out a box and bending a knee in public is for actors in jewelry commercials!
+
+DESIGN
+-----------------
+The design is very simple: the app, which runs as a fullscreen standalone app (as opposed to a watchface). The proposal is displayed as scrollable text.
+
+I chose Chrono Trigger, the classic SNES RPG, as a theme for two reasons: First, she and I grew up playing a lot of video games and Chrono Trigger was always a favorite of ours. We discovered this fact the night we met, much to our mutual delight. Second, the Chrono (time) theme was apt considering the fact that the app was to run on a watch.
+
+Enjoy!
---
+++ src/chrono.c
@@ -0,0 +1,70 @@
+#include "pebble_os.h"
+#include "pebble_app.h"
+#include "pebble_fonts.h"
+
+
+#define MY_UUID { 0x76, 0x9F, 0x72, 0xA8, 0x88, 0x51, 0x4A, 0xA1, 0xB8, 0xD9, 0xFB, 0x74, 0xF5, 0x45, 0xE2, 0xA7 }
+PBL_APP_INFO(MY_UUID,
+             "Chrono", "Rick Ucker",
+             1, 0, /* App version */
+             RESOURCE_ID_MASAMUNE_WHITE,
+             APP_INFO_STANDARD_APP);
+
+static const char * CRONO_SCRIPT_TEXT="I love you very much and I want the world to know that we're a team. Let's take on the world together. Let's make it official. Will you be my Lucca?";
+
+Window window;
+BmpContainer image_container_crono;
+BmpContainer image_container_lucca;
+TextLayer textLayer;
+ScrollLayer scrollLayer;
+void handle_init(AppContextRef ctx) {
+  GSize text_area_size = GSize(144, 2000);
+  window_init(&window, "Main");
+  window_set_fullscreen(&window, true);
+  window_set_background_color(&window, GColorBlack);
+  window_stack_push(&window, true /* Animated */);
+  //Crono
+  resource_init_current_app(&CHRONO_IMAGE_RESOURCES);
+  bmp_init_container(RESOURCE_ID_CRONO_BLACK, &image_container_crono);
+  bitmap_layer_set_alignment(&image_container_crono.layer, GAlignCenter);
+  layer_set_frame((Layer *)&image_container_crono.layer, GRect(0,0,72,70));
+  layer_add_child(&window.layer, (Layer *)&image_container_crono.layer);
+  //Lucca
+  resource_init_current_app(&CHRONO_IMAGE_RESOURCES);
+  bmp_init_container(RESOURCE_ID_LUCCA_BLACK, &image_container_lucca);
+  bitmap_layer_set_alignment(&image_container_lucca.layer, GAlignCenter);
+  layer_set_frame((Layer *)&image_container_lucca.layer, GRect(72,0,72,70));
+  layer_add_child(&window.layer, (Layer *)&image_container_lucca.layer);
+
+  text_layer_init(&textLayer, GRect(0,0,144,88));
+  text_layer_set_text_color(&textLayer, GColorWhite);
+  text_layer_set_background_color(&textLayer, GColorBlack);
+  text_layer_set_font(&textLayer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text(&textLayer, CRONO_SCRIPT_TEXT);
+  text_layer_set_size(&textLayer, text_area_size);
+
+  scroll_layer_init(&scrollLayer, GRect(0,70,144,88));
+  scroll_layer_set_click_config_onto_window(&scrollLayer, &window);
+  scroll_layer_set_content_size(&scrollLayer, text_area_size);
+  scroll_layer_add_child(&scrollLayer, &textLayer.layer);
+
+  GSize used_size = text_layer_get_max_used_size(app_get_current_graphics_context(), &textLayer);
+  GSize max_size = GSize(used_size.w, used_size.h + 4);
+  text_layer_set_size(&textLayer, max_size);
+  scroll_layer_set_content_size(&scrollLayer, max_size);
+  layer_add_child(&window.layer, &scrollLayer.layer);
+}
+
+void handle_deinit(AppContextRef ctx) {
+ bmp_deinit_container(&image_container_crono);
+ bmp_deinit_container(&image_container_lucca);
+}
+
+
+void pbl_main(void *params) {
+  PebbleAppHandlers handlers = {
+    .init_handler = &handle_init,
+    .deinit_handler = &handle_deinit
+  };
+ app_event_loop(params, &handlers);
+}
